3ae30dc8147e60deb3e4a410fd16d8b3c8d00ba0
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-03-30 00:46:12 +0530

    Update anagram.c

--- anagram.c
+++ anagram.c
@@ -4,5 +4,33 @@ int main()
 {
     chat str1[100],str2[100];
     int i;
-    prinf()
+    prinf("enter the first string: ");
+    fgets(str1,sizeof(str1),stdin);
+
+    printf("Enter the second string:  ");
+    fgets(str1,sizeof(str2),stdin);
+
+    len1 = 0, len2 = 0 ;
+    while (strlen(str1) != 0 )
+    {
+        len1++;
+    }
+
+    while (strlen(str2) != 0)
+    {
+        len2++;
+    }
+
+    if (str1(len-1)=='\n')
+    {
+        str1[len1-1=0];
+        len--;
+    }
+    if (str2(len2-1)=='\n')
+    {
+        str2[len2-1];
+        len --;
+    }
+
+
 }
