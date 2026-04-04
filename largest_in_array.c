e7173c5c02d57f598429e8c9a9dda90a294b46c6
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-04 13:05:55 +0530

    commit

--- largest_in_array.c
+++ largest_in_array.c
@@ -9,6 +9,7 @@
         max = array[i];
     }
  }
+ array[0] = 999;
  return max;
  }
  
@@ -17,5 +18,9 @@ int main()
     int arr[] = {1,2,3,4,5,6,7,8,9,12,13};//agar yha change karege to upar v changed array hi pass hoga with the help of pointer
     int max = returnMax(arr,11);//this is local variable 
     printf("the maximum element in this array is %d",max);
+    for(int i = 0; i < 7; i++)
+    {
+        printf("The element %d is %d\n",i,arr[i]);
+    }
     return 0;
 }
