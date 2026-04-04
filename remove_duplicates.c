504747c975348a6959ea93a7fb3d7208010bbed2
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-04 12:12:26 +0530

    commit

--- remove_duplicates.c
+++ remove_duplicates.c
@@ -3,6 +3,26 @@
  int main()
  {
     char str[100];
+    int seen[256] = {0};
     printf("Enter the characters: ");
-    
+    fgets(str,sizeof(str),stdin);
+    printf("without duplicates: ");
+
+    for(int i = 0; str[i] !='\0'; i++)
+    {
+        if (str[i] == '\n')//check for new line 
+        {
+            continue;
+        }
+        unsigned char c = tolower(str[i]);
+        if(seen[c] == 0)
+        {
+            printf("%c",str[i]);
+            seen[c] = 1;
+        
+        }
+    }
+    printf("\n"); 
+    return 0;
+
  }
