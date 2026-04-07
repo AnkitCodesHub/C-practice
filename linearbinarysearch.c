10c97a99cfc9e7083cadec69d327dbce2a277043
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-07 23:25:51 +0530

    commit

--- linearbinarysearch.c
+++ linearbinarysearch.c
@@ -3,7 +3,7 @@ int linearSearch(int arr[],int size, int element){
     for(int i = 0; i < size; i++)
     {
         if (arr[i] == element){
-            return 1;
+            return i;
         }
     }  
     return -1;
@@ -13,8 +13,8 @@ int main()
 {
     int arr[] = {1,3,5,6,7,8,9,10,24,4,56,67};
     int size = sizeof(arr)/sizeof(int);
-    int element = 4;
-    int searchIndex = linearSearch(arr,size,4);
+    int element = 67;
+    int searchIndex = linearSearch(arr,size,element);
     printf("The element %d was found at index %d \n",element, searchIndex);
     return 0;
 }
