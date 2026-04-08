779e69a2f3abfaac6a4a1fcf8b176683954d55be
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-08 13:06:38 +0530

    commit

--- linearsearch.c
+++ linearsearch.c
@@ -1,8 +1,8 @@
 // a program in c to perform linear search 
 #include<stdio.h>
-int linearSearch(int arr[],int size,int element)
+int linearSearch(int arr[],int n,int element)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < n; i++)
     {
         if (arr[i] == element)
         {
@@ -17,7 +17,7 @@ int main()
     int arr [] = {10,12,25,26,76,85};
     int n = sizeof[arr]/sizeof(arr[0]);
     int target = 76;
-    int searchIndex =  linearSeach(arr, size, element);
+    int searchIndex =  linearSearch(arr, n, element);
     printf("The element %d is at index %d",element,searchIndex);
     return 0;
     
