4ad99d91175d8038aa571a2b23e82ce5852b6142
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-09 22:14:53 +0530

    commit

--- bubblesort.c
+++ bubblesort.c
@@ -14,6 +14,8 @@ void bubbleSort(int* a,int n)
     int temp;
     for(int i = 0 ; i < n; i++)//for number of passes
     {
+        printf("Working on pass number %d\n",i+1); 
+        isSorted =1;
         for(int j = 0; j <n-1-i; j++)//for comparison in each pass
         {
             if(a[j]>a[j+1])
@@ -21,8 +23,12 @@ void bubbleSort(int* a,int n)
                 temp = a[j];
                 a[j] = a[j+1];
                 a[j+1] = temp; 
+                isSorted = 0;
 
             }
+        }
+        if (isSorted){
+            return;
         }
     }
 }
@@ -31,7 +37,7 @@ int main()
 {
     int a[] = {12, 54, 65, 7, 23,  9};
     // int a[] = {1,2,3,4,5,6,7,8,9};
-    int n = 6;
+    int n = sizeof(a)/sizeof(a[0]) ;
     printArray(a, n);//printing the array before sort
     bubbleSort(a,n);//function to sort the array
     printArray(a, n);//printing the array after sort
