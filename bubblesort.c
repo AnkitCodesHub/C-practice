98f6c96d401e472f8dabbf1346fb2fc6fe0193a5
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-09 12:15:34 +0530

    commit

--- bubblesort.c
+++ bubblesort.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
-void printArray(int A, int n)
+void printArray(int* a, int n)
 {
     for(int i=0; i<n;i++)
     {
-        printf("%d",a[i])
+        printf("%d",a[i]);
     }
 }
+void bubbleSort(a,n)
 
 int main()
 {
-    int a[] = {12,54,65,7,23,9}
+    int a[] = {12, 54, 65, 7, 23,  9};
     int n = 6;
-    printArray(a, n);
+    printArray(a, n);//printing the array before sort
+    bubbleSort(a,n);//function to sort the array
+    printArray(a, n);//printing the array after sort
     return 0;
 }
