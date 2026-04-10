54b16c680e04603a1b7f1ee634a93ca4cc457d7e
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-10 13:22:01 +0530

    commit

--- selectiosort.c
+++ selectiosort.c
@@ -10,10 +10,19 @@ void printArray(int *A, int n)
 }
 void selectioSort(int *A, int n)
 {
+    int indexOfMin;
     printf("Running selection sort...\n");
+    for(int i =0;i<n-1;i++)
+    {
+         
+    }
 }
 int main()
-{
+{ 
+    // input array
+    // 00 01 02 03 04 05 
+    // 3   4  5  6  9 11
+    // After first pass 
     int A[] = {3, 4, 5, 6, 9, 11};
     int n = sizeof(a)/sizeof(a[0]);
     printArray(A, n);
