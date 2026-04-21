79bfe77740d423d5d07fe7783d5b0107de10e441
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-21 14:01:14 +0530

    commit

--- insertionsort.c
+++ insertionsort.c
@@ -1,18 +1,26 @@
+// Ankit Raj Date:21,Apr,2026 Insertion Sort
 #include<stdio.h>
 void insertionSort (int *a, int n)
 {
     for (int i =0; i<n; i++)
     {
-        printf("%d",a[i]);
+        printf("%d ", a[i]);
     }
     printf("\n");
+}
+void insertionSort(int *a, int n)
+{
+   for (int i = 0; i < n ; i++) 
+   {
+
+   }
 }
 int main ()
 {
     // 4 5 6 7 8 2 3
     int a[] = {12, 54, 65, 60, 23, 9};
-    int n = sizeof (a)/sizeof(a[0]);
-    printArray(a);
-    // insertionSort(A, n)
+    int n = sizeof(a)/sizeof(a[0]);
+    // printArray(a , n);
+    insertionSort(a, n);
     return 0;
 }
