3bafb5ef441a91ac4168a5874a9a1b5af3d5c2d9
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-21 15:57:55 +0530

    commit

--- insertionsort.c
+++ insertionsort.c
@@ -1,6 +1,6 @@
 // Ankit Raj Date:21,Apr,2026 Insertion Sort
 #include<stdio.h>
-void insertionSort (int *a, int n)
+void printArray(int *a, int n)
 {
     for (int i =0; i<n; i++)
     {
@@ -10,17 +10,45 @@ void insertionSort (int *a, int n)
 }
 void insertionSort(int *a, int n)
 {
-   for (int i = 0; i < n ; i++) 
+    int key, j;
+    // loop for passes
+   for (int i = 1; i <= n-1 ; i++) 
    {
-
+    key  = a[i];
+    j = i-1;
+    while (j>=0 && a[j] > key)
+    {
+        a[j+1] = a[j];
+        j--; 
+    }
+    a[j+1] = key;
    }
 }
 int main ()
 {
-    // 4 5 6 7 8 2 3
+     // -1   0    1   2   3   4   5
+   //      12,| 54, 65, 07, 23, 09 --> i=1, key=54, j=0
+   //      12,| 54, 65, 07, 23, 09 --> 1st pass done (i=1)!
+
+   //      12, 54,| 65, 07, 23, 09 --> i=2, key=65, j=1
+   //      12, 54,| 65, 07, 23, 09 --> 2nd pass done (i=2)!
+
+   //      12, 54, 65,| 07, 23, 09 --> i=3, key=7, j=2
+   //      12, 54, 65,| 65, 23, 09 --> i=3, key=7, j=1
+   //      12, 54, 54,| 65, 23, 09 --> i=3, key=7, j=0
+   //      12, 12, 54,| 65, 23, 09 --> i=3, key=7, j=-1
+   //      07, 12, 54,| 65, 23, 09 --> i=3, key=7, j=-1--> 3rd pass done (i=3)!
+
+   // Fast forwarding and 4th and 5th pass will give:
+   //      07, 12, 54, 65,| 23, 09 --> i=4, key=23, j=3
+   //      07, 12, 23, 54,| 65, 09 --> After the 4th pass
+
+   //      07, 12, 23, 54, 65,| 09 --> i=5, key=09, j=4
+   //      07, 09, 12, 23, 54, 65| --> After the 5th pass
     int a[] = {12, 54, 65, 60, 23, 9};
     int n = sizeof(a)/sizeof(a[0]);
-    // printArray(a , n);
+    printArray(a, n);
     insertionSort(a, n);
+    printArray(a, n);
     return 0;
 }
