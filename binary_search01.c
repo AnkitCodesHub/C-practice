c9b252cef606babd10346b76058274de6d585172
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-26 22:40:41 +0530

    commit

--- binary_search01.c
+++ binary_search01.c
@@ -2,16 +2,36 @@
 
 int binarySearch(int arr[], int size, int element)
 {
- if (arr[i] == element)
- {
-    return i;
- }
- return -1;
+int low,mid,high;
+low = 0;
+high = size -1;
+ while(low<=high)
+{
+    
+    mid = (low + high)/2;
+   
+    if (arr[mid] == element)
+        {
+            return mid;
+        }
+    else if (arr[mid]<element)
+        {
+            low = mid+1;
+        }
+    else
+        {
+            high = mid-1;
+        }
+    }
+return -1;
 }
 int main()
 {
-    int arr[]={1,2,3,4,5,,6,7,8,9,10};
-    int size = sizeof(arr)/sizeof(int);
-    int element = 5
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int size = sizeof(arr)/sizeof arr[0];
+    int element = 5;
+    int searchIndex = binarySearch(arr,size,element);
+    printf("The element %d is on %dth index\n ",element,searchIndex);
+    return 0;
     
 }
