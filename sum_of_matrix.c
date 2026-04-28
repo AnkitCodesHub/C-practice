212cd6138118a85747bfe18c09db56583b2783f9
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-28 23:52:45 +0530

    commit

--- sum_of_matrix.c
+++ sum_of_matrix.c
@@ -8,7 +8,7 @@ int main()
     {
         for (j=0;j<3;j++)
         {
-            scanf("%d",&a[i][j]);
+            scanf("%d ",&a[i][j]);
         }
         printf("\n");
     }
@@ -17,7 +17,7 @@ int main()
     {
         for (j=0;j<3;j++)
         {
-            scanf("%d",&a[i][j]);
+            printf("%d ",a[i][j]);
         }
         printf("\n");
     }
@@ -26,7 +26,7 @@ int main()
     {
         for (j=0;j<3;j++)
         {
-            scanf("%d",&b[i][j]);
+            scanf("%d ",&b[i][j]);
         }
         printf("\n");
     }
@@ -35,7 +35,7 @@ int main()
     {
         for (j=0;j<3;j++)
         {
-            scanf("%d",&b[i][j]);
+            printf("%d ",b[i][j]);
         }
         printf("\n");
     }
