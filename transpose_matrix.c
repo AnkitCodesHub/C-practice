0390f182b8433287c3243d95abed9a44b48da7ee
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-04-29 16:18:02 +0530

    commit

--- transpose_matrix.c
+++ transpose_matrix.c
@@ -1,6 +1,30 @@
 #include<stdio.h>
 int main()
 {
+    // initialising the integers
     int a[2][3],i,j;
-    printf("enter the elements of matrix")
+    printf("enter the elements of matrix");
+    // checking the values
+    for(i=0;i<2;i++)
+    {
+      for(j=0;j<3;j++)
+      {
+        scanf("%d ",&a[i][j]);
+      }  
+    }
+    // for printing the value of matrix
+      for(i=0;i<2;i++)
+    {
+      for(j=o;j<3;j++)
+      {
+        scanf("%d ",&a[i][j]);
+      }  
+    }
+      for(i=0;i<2;i++)
+    {
+      for(j=o;j<3;j++)
+      {
+        scanf("%d ",&a[i][j]);
+      }  
+    }
 }
