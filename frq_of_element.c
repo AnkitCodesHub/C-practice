314ccc23284a4170deaf054ac1bbb6f20c7f9b88
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-07-01 00:34:30 +0530

    Add initial frequency of elements program

--- frq_of_element.c
+++ frq_of_element.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a[10];f[10],n,i,j;
+    int a[10],f[10],n,i,j;
     printf("\n enter the number of values\n");
-    scanf("%d ",&n);
+    scanf("%d",&n);
     printf("\n Enter values\n");
     for(i=0;i<n;i++)
     {
