e18556d3c7c63ceb06a46be2ece6f0955b9d0a25
Ankit Pandey <hrishabkumarpandey022@gmail.com>
2026-07-16 18:10:01 +0530

    commit

--- 25_frq_of_element.c
+++ 25_frq_of_element.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int a[10],f[10],n,i,j,count;
+    int a[10],f[10]={0},n,i,j,count;
     int visited = -1;
     printf("\n enter the number of values\n");
     scanf("%d",&n);
+
     printf("\n Enter values\n");
-    
     for (i=0; i<n; i++)
     {
         scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++)
     {
+        if (f[i] == visited) continue;
        count = 1;
         for (j=i+1;j<n;j++)
         {    
             if (a[i]==a[j])
             {
                 count ++;
+                f[j] = visited;
             }
         }
         f[i] = count;
@@ -26,7 +28,8 @@ int main()
     printf("\nFrequencies:\n");
     for( i=0;i<n;i++)
     {
-        printf("%d occured %d times",a[i],f[i]);
+        if (f[i] == visited) continue;
+        printf("%d occured %d times\n",a[i],f[i]);
     }
     return 0;
 }
